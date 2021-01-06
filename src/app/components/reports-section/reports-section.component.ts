e9b26a86b6a162f46e184f76c64bdc7fe306e8d6
Agustín La Valla <agulavalla@gmail.com>
2021-01-06 14:40:33 -0300

    Data changed

--- src/app/components/reports-section/reports-section.component.ts
+++ src/app/components/reports-section/reports-section.component.ts
@@ -8,15 +8,15 @@ import { Component, OnInit } from '@angular/core';
 export class ReportsSectionComponent implements OnInit {
 
   reportArray = [
-    {title: 'Gross Sale', price: "$2,456.45"},
-    {title: 'Gross Sale', price: "$2,456.45"},
-    {title: 'Gross Sale', price: "$2,456.45"},
-    {title: 'Gross Sale', price: "$2,456.45"},
-    {title: 'Gross Sale', price: "$2,456.45"},
-    {title: 'Gross Sale', price: "$2,456.45"},
-    {title: 'Gross Sale', price: "$2,456.45"},
-    {title: 'Gross Sale', price: "$2,456.45"},
-    {title: 'Gross Sale', price: "$2,456.45"},
+    {title: 'Gross Sale', info: "$2,456.45"},
+    {title: 'Net Sales', info: "$2,456.45"},
+    {title: 'Total Customers', info: "453"},
+    {title: 'Total Covers', info: "$2,456.45"},
+    {title: 'Total Orders', info: "2,453"},
+    {title: 'Total Employees', info: "453"},
+    {title: 'Total Taxes', info: "86.50%"},
+    {title: 'Total Tips', info: "2.453"},
+    {title: 'Total Returning Custumees', info: "453"},
   ]
 
   constructor() { }
